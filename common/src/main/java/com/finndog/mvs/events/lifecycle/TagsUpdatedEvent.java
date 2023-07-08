6856ab3a0b85da9e7fbafb7932d3b20cceff053e
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2023-07-08 17:40:18 +0100

    changed mod ID to MES, Moog's End Structures

--- common/src/main/java/com/finndog/mvs/events/lifecycle/TagsUpdatedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.finndog.mvs.events.lifecycle;
-
-import com.finndog.mvs.events.base.EventHandler;
-import net.minecraft.core.RegistryAccess;
-
-public record TagsUpdatedEvent(RegistryAccess registryAccess, boolean fromPacket) {
-
-    public static final EventHandler<TagsUpdatedEvent> EVENT = new EventHandler<>();
-}
