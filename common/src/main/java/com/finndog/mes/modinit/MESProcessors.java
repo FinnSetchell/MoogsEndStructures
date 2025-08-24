c01820d0413c5a4df178c2d27277578c0bb781e0
finnsetchell <finn@setchell.org>
2025-08-24 14:45:16 +0200

    fixes and changes after cherry picking

--- common/src/main/java/com/finndog/mes/modinit/MESProcessors.java
+++ common/src/main/java/com/finndog/mes/modinit/MESProcessors.java
@@ -8,6 +8,4 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProc
 
 public final class MESProcessors {
     public static final ResourcefulRegistry<StructureProcessorType<?>> STRUCTURE_PROCESSOR = ResourcefulRegistries.create(BuiltInRegistries.STRUCTURE_PROCESSOR, MESCommon.MODID);
-
-    //public static final RegistryEntry<StructureProcessorType<WaterloggingFixProcessor>> WATERLOGGING_FIX_PROCESSOR = STRUCTURE_PROCESSOR.register("waterlogging_fix_processor", () -> () -> WaterloggingFixProcessor.CODEC);
 }
